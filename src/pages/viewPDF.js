608470f30c7710809b1cbffe5b88760fd6c1b9ad
Haziq Musa <last.game01987@gmail.com>
2022-01-07 18:28:49 +0800

    update viewPDF

--- src/pages/viewPDF.js
+++ src/pages/viewPDF.js
@@ -4,7 +4,7 @@ import pdf from "../file/laporan-kumpulan-3.pdf";
 const viewPDF = () => {
   return (
     <div className="view">
-      <embed src={pdf} />
+      <embed src={pdf} type="application/pdf" />
     </div>
   );
 };
